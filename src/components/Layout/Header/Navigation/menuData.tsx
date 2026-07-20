694ff9421e09d7d4b96efa3e821c079dfff48396
Kiarash :) <107726105+kiarashmiri22@users.noreply.github.com>
2026-07-21 00:38:34 +0330

    Delete src directory

--- src/components/Layout/Header/Navigation/menuData.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderItem } from "@/types/menu";
-
-export const headerData: HeaderItem[] = [
-  { label: "Skills", href: "/#work" },
-  { label: "Process", href: "/#development" },
-  { label: "Projects", href: "/#portfolio" },
-  { label: "FAQ", href: "/#faq" },
-  { label: "Contact", href: "/#contact" },
-];
